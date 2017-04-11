1e159bc2ac162d91b64f947b5db5d286e5215210
chenghx <chenghx9211@163.com>
2017-04-11 15:09:31 +0800

    add release config file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,2 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 export * from './components/upload.vue'
